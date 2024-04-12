da7c02b85e94b718297d5a4527ead83621b317bb
NKMAC27 <156964308+NKMAC27@users.noreply.github.com>
2024-04-12 14:08:51 -0500

    i tried

--- Running_Calculator.py
+++ Running_Calculator.py
@@ -23,33 +23,39 @@ class First_Window_GUI:
         instruction_1.grid(row=1, column=4, columnspan=4)
 
         time = tk.Label(master=self.root, text="Time:",
-                              font='Arial 15', bd=5, relief='groove')
+                        font='Arial 15', bd=5, relief='groove')
         time.grid(row=2, column=0, columnspan=4, pady=10)
 
         distance = tk.Label(master=self.root, text="Distance:",
-                              font='Arial 15', bd=5, relief='groove')
+                            font='Arial 15', bd=5, relief='groove')
         distance.grid(row=3, column=0, columnspan=4, pady=10)
 
         pace = tk.Label(master=self.root, text="Pace:",
-                                  font='Arial 15', bd=5, relief='groove')
+                        font='Arial 15', bd=5, relief='groove')
         pace.grid(row=4, column=0, columnspan=4, pady=10)
 
-        distance_input = tk.Entry(textvariable=self.userDistance, font='Arial 16', bd=1,
-                               exportselection=0, width=25, justify='center')
+        distance_input = tk.Entry(self.root, bd=1, font="Arial 16",
+                                  justify=tk.CENTER, width=24)
         distance_input.grid(row=3, column=4)
 
+        pace_minute_input = tk.Entry(self.root, bd=1, font="Arial 16",
+                              justify=tk.CENTER, width=12)
+        pace_minute_input.grid(row=4, column=4)
 
-
-
-
-
-
+        pace_seconds_input = tk.Entry(self.root, bd=1, font="Arial 16",
+                                     justify=tk.CENTER, width=12)
+        pace_seconds_input.grid(row=4, column=5)
 
     def goProgram(self):
         self.root.mainloop()
 
 
-
+"""
+Amin said to try using the Frame widget. It puts a grid that we can manipulate within the tkinter grid.
+Could help us center stuff.
+Also said we could download Bootstrap to make it look nicer. Says the commands are all the same, but 
+I don't think we need that.
+"""
 
 
 
